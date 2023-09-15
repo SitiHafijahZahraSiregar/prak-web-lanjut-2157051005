7f3c12787276c9de5aaac3516de3cc545812c9fb
SitiHafijahZahraSiregar <sitihafizah579@gmail.com>
2023-09-15 17:08:57 +0700

    routing dan View

---
+++ app/Views/profile.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="text-center">
+<img src="https://wallpapercave.com/wp/wc1732595.jpg" class="rounded-circle" alt="..." width="200" height="200">
+<table>
+<p>
+<tr>
+<div class="alert alert-danger" role="alert">
+  Siti Hafijah Zahra Siregar
+</div>
+</tr>
+<tr>
+<div class="alert alert-danger" role="alert">
+  AB
+</div>
+</tr>
+<tr>
+<div class="alert alert-danger" role="alert">
+  2157051005
+</div>
+</tr>
+</p>
+</table>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</div>
+</body>
+</html>
